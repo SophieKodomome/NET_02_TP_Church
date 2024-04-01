fa2d7567fdfb02ff8d0bd55e8574728a5638b2ef
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-04-01 16:35:19 +0300

    Update StaticExpense.cs

--- Church/StaticExpense.cs
+++ Church/StaticExpense.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 namespace Expenses
 {
     public class StaticExpense
@@ -23,6 +25,29 @@ namespace Expenses
         {
             MontantUnitaire=montantUnitaire;
             return this;
+        }
+        private static List<StaticExpense> GetStaticExpenses(NpgsqlConnection connection)
+        {
+            List<StaticExpense> listStaticExpense = new List<StaticExpense>();
+
+            connection.Open();
+            using(var command=new NpgsqlCommand("select * from static_expenses",connection))
+            {
+                using(var reader=command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        StaticExpense staticExpense = new StaticExpense().
+                        addId(reader.GetInt32(0)).
+                        addMotif(reader.GetString(1)).
+                        addMontantUnitaire(reader.GetInt32(2));
+
+                        listStaticExpense.Add(staticExpense);
+                    }
+                }
+            }
+            connection.Close();
+            return listStaticExpense;
         }
     }
 }
