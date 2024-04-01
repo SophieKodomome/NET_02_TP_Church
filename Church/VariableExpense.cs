d3aacc2ee607c0f73448bea501cfff0390830d2e
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-04-01 16:19:21 +0300

    Update VariableExpense.cs

--- Church/VariableExpense.cs
+++ Church/VariableExpense.cs
@@ -2,17 +2,14 @@ namespace Expenses
 {
     public class VariableExpense
     {
-        private int id;
-        private string motif;
-        private int montantUnitaire;
-
-        private string nomVariable;
-        private int variable;   
         public int Id{get;set;}
+        public int IdVariableExpense{get;set;}
         public string Motif{get;set;}
         public int MontantUnitaire{get;set;}
-        public string MotifVariable{get;set;}
+        public string UniteMesureVariable{get;set;}
         public int Variable{get;set;}
+
+        public int Mois{get;set;}
 
 
         public VariableExpense(){}
@@ -20,6 +17,12 @@ namespace Expenses
         public VariableExpense addId(int id)
         {
             Id=id;
+            return this;
+        }
+
+        public VariableExpense addIdVariableExpense(int idVariableExpense)
+        {
+            IdVariableExpense=idVariableExpense;
             return this;
         }
         public VariableExpense addMotif(string motif)
@@ -32,14 +35,20 @@ namespace Expenses
             MontantUnitaire=montantUnitaire;
             return this;
         }
-        public VariableExpense addMotifVariable(string motifVariable)
+        public VariableExpense addUniteMesureVariable(string uniteMesureVariable)
         {
-            MotifVariable=motifVariable;
+            UniteMesureVariable=uniteMesureVariable;
             return this;
         }
         public VariableExpense addVariable(int variable)
         {
             Variable=variable;
+            return this;
+        }
+
+        public VariableExpense addMois(int mois)
+        {
+            Mois=mois;
             return this;
         }
     }
