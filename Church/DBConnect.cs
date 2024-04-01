d7b952d570927cf4d193f176b47ce25d3cc0e647
Peeter Randria <126390972+SophieKodomome@users.noreply.github.com>
2024-04-01 12:36:48 +0300

    add DBConnect

---
+++ Church/DBConnect.cs
@@ -0,0 +1,42 @@
+using System;
+using Npgsql;
+
+namespace connect
+{
+
+    public class DBConnect
+    {
+        private string test;
+        String connectionString;
+
+        public string Test { get; set; }
+        public string ConnectionString {get;set;}
+        public DBConnect()
+        {
+            Init_Connection();
+        }
+
+        private void Init_Connection()
+        {
+            ConnectionString="Host=localhost;Port=5432;Database=pharmacie;Username=pharmacie;Password=pharmacie;";
+
+            using var connection = new NpgsqlConnection(ConnectionString);
+
+            try
+            {
+                connection.Open();
+                Test = "Connected to PostgreSQL!";
+                //Console.WriteLine("Connected to PostgreSQL!");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Error: {ex.Message}" );
+                Test = $"Error: {ex.Message}";
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
